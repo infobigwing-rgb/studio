0c5b5b0caa413ee28115619ad709b630b7b467e0
Sheikh Md Junaeid <tjmskp@gmail.com>
2025-11-01 21:57:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-881852331-4bea4",
+  "appId": "1:173990783981:web:cb8f8db0955012184115e4",
+  "apiKey": "AIzaSyBe8JZC8PsfJ9s0kk45jJLX_6Xgwj6rj9s",
+  "authDomain": "studio-881852331-4bea4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "173990783981"
+};
